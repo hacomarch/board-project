f5337c00c978a7e37eb6571724218a5a0f4eeebe
윤하은 <haco_march@naver.com>
2023-09-13 20:06:34 +0900

    대댓글 기능에 따른 댓글 도메인 변경

--- src/main/java/com/fastcampus/fastcampusprojectboard/domain/ArticleComment.java
+++ src/main/java/com/fastcampus/fastcampusprojectboard/domain/ArticleComment.java
@@ -5,7 +5,9 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.util.LinkedHashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Getter
 @ToString(callSuper = true)
@@ -26,18 +28,33 @@ public class ArticleComment extends AuditingFields{
     @Setter @ManyToOne(optional = false) @JoinColumn(name = "userId")
     private UserAccount userAccount;
 
+    @Setter
+    @Column(updatable = false)  //한 번 세팅되면, 업데이트가 되지 않게 설정
+    private Long parentCommentId;   //부모 댓글 아이디
+
+    @ToString.Exclude
+    @OrderBy("createdAt ASC")
+    @OneToMany(mappedBy = "parentCommentId", cascade = CascadeType.ALL)
+    private Set<ArticleComment> childComments = new LinkedHashSet<>();
+
     @Setter @Column(nullable = false, length = 500) private String content;
 
     protected ArticleComment() {}
 
-    private ArticleComment(Article article, UserAccount userAccount, String content) {
+    private ArticleComment(Article article, UserAccount userAccount, Long parentCommentId, String content) {
         this.article = article;
         this.userAccount = userAccount;
+        this.parentCommentId = parentCommentId;
         this.content = content;
     }
 
     public static ArticleComment of(Article article, UserAccount userAccount, String content) {
-        return new ArticleComment(article, userAccount, content);
+        return new ArticleComment(article, userAccount, null, content);
+    }
+
+    public void addChildComment(ArticleComment child) {
+        child.setParentCommentId(this.getId());
+        this.getChildComments().add(child);
     }
 
     @Override
